b308d789ced623eba7870382118eb9dd86ab1631
Fernando Lira <local@fernand.us>
2022-07-12 20:29:51 -0300

    adjusted env/env

--- config/application.php
+++ config/application.php
@@ -9,7 +9,7 @@ $webroot_dir = $root_dir . '/web';
 /**
  * Expose global env() function from oscarotero/env
  */
-Env::init();
+use Env\Env;
 
 /**
  * Use Dotenv to set required environment variables and load .env file in root
